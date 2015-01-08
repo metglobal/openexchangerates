cfac0cd2bc743ae18bd35ef92c421f13e1f523cb
kadirpekel <kadirpekel@gmail.com>
2015-01-08 16:39:03 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='openexchangerates',
-    version='0.1.0',
+    version='0.1.1',
     description='openexchangerates.org python API client',
     long_description=open('README.rst').read(),
     url='https://github.com/metglobal/openexchangerates',
